ba434e50e45ffa85d4d2e6c6c4a69a4a9adff47c
ekeemmanuel <emmazy100@gmail.com>
2025-01-01 11:29:17 +0100

    ad

--- pages/salesrep/dashboard.js
+++ pages/salesrep/dashboard.js
@@ -37,7 +37,7 @@ export default function SalesRepDashboard() {
 
 
 
-  
+
 
 
 
@@ -135,7 +135,7 @@ export default function SalesRepDashboard() {
     }
   };
 
-  
+
 
   const fetchGoods = async () => {
     try {
@@ -154,46 +154,46 @@ export default function SalesRepDashboard() {
 
 
 
-  
-// Inside your SalesRepDashboard component
-const handleScanForGoods = () => {
-  const html5QrCode = new Html5Qrcode("qr-reader");
-  const qrCodeSuccessCallback = async (decodedText) => {
-    try {
-      // Fetch product details from Firestore by productId
-      const productRef = collection(firestore, "goods");
-      const productSnapshot = await getDocs(productRef);
-      const goodsList = productSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      const scannedProduct = goodsList.find(
-        (item) => item.productId === decodedText
-      );
 
-      if (scannedProduct) {
-        addToCart(scannedProduct); // Add scanned product to the cart
-        console.log(`Product "${scannedProduct.name}" added to cart.`);
-      } else {
-        console.error("Product not found.");
+  // Inside your SalesRepDashboard component
+  const handleScanForGoods = () => {
+    const html5QrCode = new Html5Qrcode("qr-reader");
+    const qrCodeSuccessCallback = async (decodedText) => {
+      try {
+        // Fetch product details from Firestore by productId
+        const productRef = collection(firestore, "goods");
+        const productSnapshot = await getDocs(productRef);
+        const goodsList = productSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        const scannedProduct = goodsList.find(
+          (item) => item.productId === decodedText
+        );
+
+        if (scannedProduct) {
+          addToCart(scannedProduct); // Add scanned product to the cart
+          console.log(`Product "${scannedProduct.name}" added to cart.`);
+        } else {
+          console.error("Product not found.");
+        }
+      } catch (error) {
+        console.error("Error fetching scanned product:", error);
       }
-    } catch (error) {
-      console.error("Error fetching scanned product:", error);
-    }
-    // Do not stop scanning; allow continuous scanning
+      // Do not stop scanning; allow continuous scanning
+    };
+
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+    // Start scanning
+    html5QrCode.start(
+      { facingMode: "environment" }, // Use rear camera
+      config,
+      qrCodeSuccessCallback
+    ).catch((error) => {
+      console.error("QR Code scanning failed:", error);
+    });
   };
-
-  const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
-  // Start scanning
-  html5QrCode.start(
-    { facingMode: "environment" }, // Use rear camera
-    config,
-    qrCodeSuccessCallback
-  ).catch((error) => {
-    console.error("QR Code scanning failed:", error);
-  });
-};
 
 
 
@@ -212,7 +212,7 @@ const handleScanForGoods = () => {
   const addToCart = (item) => {
     const existingItem = cart.find((cartItem) => cartItem.id === item.id);
     const quantity = selectedQuantity;
-  
+
     if (existingItem) {
       setCart(
         cart.map((cartItem) =>
@@ -229,11 +229,11 @@ const handleScanForGoods = () => {
       ]);
       setTotal((prevTotal) => prevTotal + item.price * quantity);
     }
-  
+
     setShowPopup(false); // Close the popup after adding to cart
     setSelectedQuantity(1); // Reset quantity input
   };
-  
+
 
   const removeFromCart = (index) => {
     const updatedCart = [...cart];
@@ -271,9 +271,9 @@ const handleScanForGoods = () => {
       alert("Cart, amount paid, and salesRepUsername cannot be empty");
       return;
     }
-  
+
     const changeGiven = calculateChange();
-  
+
     try {
       const newSale = {
         salesRepUsername,
@@ -283,29 +283,31 @@ const handleScanForGoods = () => {
         changeGiven,
         timestamp: serverTimestamp(),
       };
-  
+
       const saleRef = await addDoc(collection(firestore, "sales"), newSale);
       setSuccess("Sale completed successfully!");
-  
+
       const batch = writeBatch(firestore);
-  
+
       cart.forEach((item) => {
         const goodsRef = doc(firestore, 'goods', item.id);
         const newStock = item.stock - item.quantity;
-  
+
         if (newStock >= 0) {
           batch.update(goodsRef, { stock: newStock });
         } else {
           alert(`Not enough stock for ${item.name}`);
         }
       });
-  
+
       await batch.commit();
-  
+
       // Generate receipt content
       const receiptContent = `
         <div>
           <h1>Receipt</h1>
+                    <h2>OMO EKO PROVISION STORE</h2>
+
           <p>Sales Rep: ${salesRepUsername}</p>
           <p>Total: ${total}</p>
           <p>Amount Paid: ${amountPaid}</p>
@@ -314,22 +316,25 @@ const handleScanForGoods = () => {
             ${cart.map(item => `<li>${item.name} - ${item.quantity} x ${item.price}</li>`).join('')}
           </ul>
           <p>Thank you for your purchase!</p>
+          <p>Goods taken in good condition can’t be returned</p>
+
         </div>
         <style>
-  body { font-family: Arial, sans-serif; padding: 20px; }
-  h1 { text-align: center; }
-  ul { list-style: none; padding: 0; }
-  li { margin: 5px 0; }
-</style>
+          body { font-family: Arial, sans-serif; padding: 20px; }
+          h1 { text-align: center; }
+          h2 { text-align: center; }
+          ul { list-style: none; padding: 0; }
+          li { margin: 5px 0; }
+        </style>
 
       `;
-  
+
       // Open a new window for the receipt and print
       const printWindow = window.open('', '_blank');
       printWindow.document.write(receiptContent);
       printWindow.document.close();
       printWindow.print();
-  
+
       // Reset the cart and other values
       setCart([]);
       setTotal(0);
@@ -340,16 +345,16 @@ const handleScanForGoods = () => {
       alert("Failed to complete sale.");
     }
   };
-  
-  
-  
+
+
+
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
 
 
 
-  
+
 
 
 
@@ -360,7 +365,7 @@ const handleScanForGoods = () => {
     <div className="min-h-screen bg-gradient-to-r from-indigo-600 to-indigo-800 p-8">
 
 
-{isMaintenanceMode && (
+      {isMaintenanceMode && (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-80 flex items-center justify-center z-50">
           <div className="bg-white p-8 rounded-lg text-center space-y-4 shadow-lg max-w-md mx-auto">
             <h1 className="text-xl font-bold text-red-600">Maintenance Mode</h1>
@@ -372,159 +377,159 @@ const handleScanForGoods = () => {
       )}
 
 
-    <div className="bg-white p-6 rounded-xl shadow-lg max-w-6xl mx-auto">
-      <h1 className="text-4xl font-semibold text-center text-black mb-8">
-        Sales Rep Dashboard
-      </h1>
-      <h2 className="text-2xl font-medium text-center text-black mb-6">
-        Welcome, {salesRepUsername || "Sales Rep"}
-      </h2>
-      {success && <h2 className="text-green-600 text-2xl font-medium text-center mb-6">{success}</h2>}
-          {error && <h2 className="text-red-600 mb-2 text-2xl font-medium text-center">{error}</h2>}
-
-  
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 ">
-        {/* Left Column */}
-        <div className="pr-20 md:border-r-4 md:border-gray-300">
-        <h3 className="text-xl font-semibold text-black mb-4">Add Sale</h3>
-        
-          <input
-                  type="text"
-                  placeholder="Search for goods..."
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4 text-black"
-                />
-<div className="overflow-y-auto max-h-64 mb-10 mt-10">
-  {filteredGoods.map((item) => (
-    <div key={item.id} className="flex justify-between mb-4 ">
-      <span className="text-black">{item.name}</span>
-      <button
-        onClick={() => addToCart(item)}
-        className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:outline-none"
-      >
-        Add to Cart
-      </button>
-    </div>
-  ))}
-</div>
-
-  
-      
-          <button
-            onClick={() => handleScanForGoods()}
-            className="w-full px-5 py-3 bg-indigo-600 text-white rounded-lg mb-6 hover:bg-indigo-700 focus:outline-none transition duration-300"
-          >
-            Scan for Goods
-          </button>
-  
-          {/* Popup for QR Code Scanner */}
-          <div id="qr-reader" className="mb-4"></div>
-  
-       
-        </div>
+      <div className="bg-white p-6 rounded-xl shadow-lg max-w-6xl mx-auto">
+        <h1 className="text-4xl font-semibold text-center text-black mb-8">
+          Sales Rep Dashboard
+        </h1>
+        <h2 className="text-2xl font-medium text-center text-black mb-6">
+          Welcome, {salesRepUsername || "Sales Rep"}
+        </h2>
+        {success && <h2 className="text-green-600 text-2xl font-medium text-center mb-6">{success}</h2>}
+        {error && <h2 className="text-red-600 mb-2 text-2xl font-medium text-center">{error}</h2>}
 
 
-  
-        {/* Right Column */}
-        <div>
-          {/* Cart */}
-          <div className="mb-6">
-            <h3 className="font-semibold text-black mb-2">Cart</h3>
-            {cart.length > 0 ? (
-              <div>
-                <ul>
-                  {cart.map((item, index) => (
-                    <li key={index} className="flex justify-between mb-4">
-                      <span className="text-black">{item.name}</span>
-                      <div className="flex items-center space-x-4">
-                        <button
-                          onClick={() => updateQuantity(index, item.quantity - 1)}
-                          className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400 focus:outline-none"
-                        >
-                          −
-                        </button>
-                        <input
-                          type="number"
-                          value={item.quantity}
-                          onChange={(e) =>
-                            updateQuantity(index, parseInt(e.target.value))
-                          }
-                          className="w-16 text-center border border-gray-300 rounded-lg text-black"
-                        />
-                        <button
-                          onClick={() => updateQuantity(index, item.quantity + 1)}
-                          className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400 focus:outline-none"
-                        >
-                          +
-                        </button>
-                      </div>
-                      <span className="font-medium text-black">
-                        #{item.price * item.quantity}
-                      </span>
-                      <button
-                        onClick={() => removeFromCart(index)}
-                        className="text-red-600 hover:text-red-700 transition duration-300"
-                      >
-                        Remove
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-                <div className="mt-4 font-semibold text-black">
-                  Total: #{total}
-                </div>
-              </div>
-            ) : (
-              <p className="text-black">No items in cart.</p>
-            )}
-          </div>
-  
-          {/* Amount Paid */}
-          <div className="mb-4">
-            <label className="block text-sm font-semibold text-black mb-2">
-              Amount Paid
-            </label>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 ">
+          {/* Left Column */}
+          <div className="pr-20 md:border-r-4 md:border-gray-300">
+            <h3 className="text-xl font-semibold text-black mb-4">Add Sale</h3>
+
             <input
-              type="number"
-              value={amountPaid}
-              onChange={(e) => setAmountPaid(parseFloat(e.target.value))}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg text-black"
+              type="text"
+              placeholder="Search for goods..."
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4 text-black"
             />
-          </div>
-  
-          {/* Complete Sale Button */}
-          <button
-            onClick={completeSale}
-            className="w-full px-5 py-3 bg-indigo-600 text-white rounded-lg mb-6 hover:bg-indigo-700 focus:outline-none transition duration-300"
-          >
-            Complete Sale
-          </button>
-  
-          {/* Display Change */}
-          {calculateChange() > 0 && (
-            <div className="mt-4 text-lg font-semibold text-black">
-              <strong>Change Given:</strong> #{calculateChange()}
+            <div className="overflow-y-auto max-h-64 mb-10 mt-10">
+              {filteredGoods.map((item) => (
+                <div key={item.id} className="flex justify-between mb-4 ">
+                  <span className="text-black">{item.name}</span>
+                  <button
+                    onClick={() => addToCart(item)}
+                    className="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:outline-none"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              ))}
             </div>
-          )}
-  
-          {/* Sales History Button */}
-          <div className="mt-6">
+
+
+
             <button
-              onClick={() => window.location.replace("/salesrep/sales")}
-              className="w-full px-5 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-900 focus:outline-none transition duration-300"
+              onClick={() => handleScanForGoods()}
+              className="w-full px-5 py-3 bg-indigo-600 text-white rounded-lg mb-6 hover:bg-indigo-700 focus:outline-none transition duration-300"
             >
-              View Sales History
+              Scan for Goods
             </button>
+
+            {/* Popup for QR Code Scanner */}
+            <div id="qr-reader" className="mb-4"></div>
+
+
+          </div>
+
+
+
+          {/* Right Column */}
+          <div>
+            {/* Cart */}
+            <div className="mb-6">
+              <h3 className="font-semibold text-black mb-2">Cart</h3>
+              {cart.length > 0 ? (
+                <div>
+                  <ul>
+                    {cart.map((item, index) => (
+                      <li key={index} className="flex justify-between mb-4">
+                        <span className="text-black">{item.name}</span>
+                        <div className="flex items-center space-x-4">
+                          <button
+                            onClick={() => updateQuantity(index, item.quantity - 1)}
+                            className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400 focus:outline-none"
+                          >
+                            −
+                          </button>
+                          <input
+                            type="number"
+                            value={item.quantity}
+                            onChange={(e) =>
+                              updateQuantity(index, parseInt(e.target.value))
+                            }
+                            className="w-16 text-center border border-gray-300 rounded-lg text-black"
+                          />
+                          <button
+                            onClick={() => updateQuantity(index, item.quantity + 1)}
+                            className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400 focus:outline-none"
+                          >
+                            +
+                          </button>
+                        </div>
+                        <span className="font-medium text-black">
+                          #{item.price * item.quantity}
+                        </span>
+                        <button
+                          onClick={() => removeFromCart(index)}
+                          className="text-red-600 hover:text-red-700 transition duration-300"
+                        >
+                          Remove
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="mt-4 font-semibold text-black">
+                    Total: #{total}
+                  </div>
+                </div>
+              ) : (
+                <p className="text-black">No items in cart.</p>
+              )}
+            </div>
+
+            {/* Amount Paid */}
+            <div className="mb-4">
+              <label className="block text-sm font-semibold text-black mb-2">
+                Amount Paid
+              </label>
+              <input
+                type="number"
+                value={amountPaid}
+                onChange={(e) => setAmountPaid(parseFloat(e.target.value))}
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg text-black"
+              />
+            </div>
+
+            {/* Complete Sale Button */}
+            <button
+              onClick={completeSale}
+              className="w-full px-5 py-3 bg-indigo-600 text-white rounded-lg mb-6 hover:bg-indigo-700 focus:outline-none transition duration-300"
+            >
+              Complete Sale
+            </button>
+
+            {/* Display Change */}
+            {calculateChange() > 0 && (
+              <div className="mt-4 text-lg font-semibold text-black">
+                <strong>Change Given:</strong> #{calculateChange()}
+              </div>
+            )}
+
+            {/* Sales History Button */}
+            <div className="mt-6">
+              <button
+                onClick={() => window.location.replace("/salesrep/sales")}
+                className="w-full px-5 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-900 focus:outline-none transition duration-300"
+              >
+                View Sales History
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  
-  
-  
-  
+
+
+
+
   );
 }
 
